dbffdc9e01580112b95e166aa50b9ca44bfc1b50
kazoottt <turbopm@outlook.com>
2024-02-19 16:34:43 +0800

    Refactor page layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -53,40 +53,46 @@ export default function Home() {
   );
   const buttonDisabled = value.trim().length === 0;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-2 lg:px-24 space-y-2">
-      <div className="flex items-center w-full max-w-xl">
-        <label className="sr-only">Confirm</label>
-        <div className="relative w-full">
-          <input
-            type="text"
-            id="simple-search"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="input the string to slugify..."
-            required
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-            onKeyDown={(event) => {
-              if (event.key === "Enter" && value.trim().length > 0) {
-                startSlugify();
-              }
-            }}
-          />
-        </div>
-        <button
-          type="submit"
-          className={`p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-2 lg:px-24 ">
+      <div className="w-full h-full max-w-xl space-y-2">
+        <div className="flex items-center w-full flex-col lg:flex-row space-y-2">
+          <label className="sr-only">Confirm</label>
+          <div className="relative w-full">
+            <input
+              type="text"
+              id="simple-search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="input the string to slugify..."
+              required
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Enter" && value.trim().length > 0) {
+                  startSlugify();
+                }
+              }}
+            />
+          </div>
+          <button
+            type="submit"
+            className={`p-2.5 lg:ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
           ${buttonDisabled && "hover:cursor-not-allowed"}`}
-          disabled={buttonDisabled}
-          onClick={startSlugify}
-        >
-          {loading ? loadingSvg : searchSvg}
+            disabled={buttonDisabled}
+            onClick={startSlugify}
+          >
+            {loading ? loadingSvg : searchSvg}
 
-          <span className="sr-only">Confirm</span>
-        </button>
+            <span className="sr-only">Confirm</span>
+          </button>
+        </div>
+        {res.trim().length > 0 && (
+          <div className="w-full text-center font-semibold text-gray-900 underline underline-offset-4 dark:text-white decoration-orange-500 decoration-wavy ">
+            {res}
+          </div>
+        )}
       </div>
-      {res.trim().length > 0 && <div>{res}</div>}
       <Link
         href="https://bento.me/kazoottt"
         className="font-semibold text-gray-900 underline underline-offset-4 dark:text-white decoration-sky-500 decoration-wavy hover:text-orange-500"
